431a36eb0a6a80c3546b1a012104117f66e33801
DevinShine <devin.xdw@gmail.com>
2016-08-23 21:07:27 +0800

    Initial Commit

---
+++ AnimationMaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationMaker
+//
+//  Created by DevinShine on 16/8/23.
+//  Copyright © 2016年 DevinShine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationMaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationMaker
+//
+//  Created by DevinShine on 16/8/23.
+//  Copyright © 2016年 DevinShine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
